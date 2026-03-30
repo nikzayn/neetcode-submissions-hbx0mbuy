fb62e61b3fb75df1f4898dfee05cb9c1731aa785
Nikhil Vaidyar <nikhilvaidyar1997@gmail.com>
2026-03-30 21:28:18 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.go
@@ -0,0 +1,24 @@
+func topKFrequent(nums []int, k int) []int {
+	freq := make(map[int]int)
+
+	for _, num := range nums {
+		freq[num]++
+	}
+
+	buckets := make([][]int, len(nums) + 1)
+
+	for k, v := range freq {
+		buckets[v] = append(buckets[v], k)
+	}
+
+	res := make([]int, 0, k)
+	for i := len(buckets) - 1; i > 0; i-- {
+		for _, v := range buckets[i] {
+			if k > 0 {
+				res = append(res, v)
+				k--
+			}
+		}
+	}
+	return res
+}
