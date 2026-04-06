33384928a311cd244e87f44b3ce2dd86bcff4711
Nikhil Vaidyar <nikhilvaidyar1997@gmail.com>
2026-04-06 11:37:36 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.go
@@ -0,0 +1,18 @@
+func subarraySum(nums []int, k int) int {
+	freq := make(map[int]int)
+
+	count, prefixSum := 0, 0
+	freq[0] = 1
+
+	for _, num := range nums {
+		prefixSum += num
+
+		if val, found := freq[prefixSum - k]; found {
+			count += val
+		}
+
+		freq[prefixSum]++ 
+	}
+
+	return count
+}
