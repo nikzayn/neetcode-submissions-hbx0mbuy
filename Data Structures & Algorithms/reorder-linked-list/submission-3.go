47c4f7437472fc2810c0b3079462fafb0ebfe6c6
Nikhil Vaidyar <nikhilvaidyar1997@gmail.com>
2026-04-09 00:17:05 +0530

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.go
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reorderList(head *ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+
+	first := head
+	mid := middle(first)
+	second := reverseList(mid.Next)
+
+	mid.Next = nil
+
+	for second != nil {
+		tmp1 := first.Next
+		tmp2 := second.Next
+
+		first.Next = second
+		second.Next = tmp1
+
+		first = tmp1
+		second = tmp2
+	}
+}
+
+func middle(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil && fast.Next.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+
+	return prev
+}
