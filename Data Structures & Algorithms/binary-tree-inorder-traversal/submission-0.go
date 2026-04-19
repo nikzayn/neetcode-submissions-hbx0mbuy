0c6a3fbadb074e5a6b3b1324bbeb357f2759b713
Nikhil Vaidyar <nikhilvaidyar1997@gmail.com>
2026-04-19 23:52:19 +0530

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	res := []int{}
+	
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+
+	inorder(root)
+	return res
+}
